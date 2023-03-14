0f1580f35ede25147dbacf55ab7c03559ad4a0a7
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-03-14 22:08:06 +0100

    Add list to initiialState

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 import styles from './NavBar.module.scss';
 
@@ -11,13 +10,10 @@ const NavBar = () => {
                         to="/Home"><span className="fa fa-tasks"></span></NavLink></li>
                         <li className={styles.item_right}><NavLink  className={({ isActive }) => isActive ? styles.linkActive : undefined }
                         to="/Home">Home</NavLink></li>
-                        <li className={styles.item_right}><NavLink  className={({ isActive }) => isActive ? styles.linkActive : undefined } to='/About'>About</NavLink></li>
-                        <li className={styles.item_right}><NavLink  className={({ isActive }) => isActive ? styles.linkActive : undefined} to='/Favorite'>Favorite</NavLink></li>
-{/* 
-                    <a href="/Home" className={styles.item_left}><span className="fa fa-tasks"></span></a>
-                    <a href="/Home" className={styles.item_right}>Home</a>
-                    <a href="/About" className={styles.item_right}>About</a>
-                    <a href="/Favorite" className={styles.item_right}>Favorite</a> */}
+                        <li className={styles.item_right}><NavLink  className={({ isActive }) => isActive ? styles.linkActive : undefined } 
+                        to='/About'>About</NavLink></li>
+                        <li className={styles.item_right}><NavLink  className={({ isActive }) => isActive ? styles.linkActive : undefined} 
+                        to='/Favorite'>Favorite</NavLink></li>
                 </ul>
             </nav>
         </div>
--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -16,7 +16,18 @@ const initialState = {
       icon: 'gamepad',
     },
   ],
-
+  lists: [
+    {
+      id: 1,
+      title: 'Things to do...',
+      description: 'Interesting things I want to check out'
+    },
+   {
+      id: 2,
+      title: 'Test list',
+      description: 'Lorem Ipsum'
+   }
+ ],
   cards: [
     { id: 1, columnId: 1, title: 'This is Going to Hurt' },
     { id: 2, columnId: 1, title: 'Interpreter of Maladies' },
@@ -25,6 +36,7 @@ const initialState = {
     { id: 5, columnId: 3, title: 'The Witcher' },
     { id: 6, columnId: 3, title: 'Skyrim' },
   ],
+  
   searchString: '',
 };
 
