3ff4333e9fcf7eaef098142471627d284c678a18
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-03-14 17:59:07 +0100

    Create navBar and remove updating of pages

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-
+import { Link } from 'react-router-dom';
 import styles from './NavBar.module.scss';
 
 const NavBar = () => {
@@ -6,10 +6,15 @@ const NavBar = () => {
         <div>
             <nav className={styles.nav}>
                 <ul className={styles.flex}>
+                    <li><Link to='/Home' className={styles.item_left}><span className="fa fa-tasks"></span></Link></li>
+                    <li><Link to='/Home' className={styles.item_right}>Home</Link></li>
+                    <li><Link to='/About' className={styles.item_right}>About</Link></li>
+                    <li><Link to='/Favorite' className={styles.item_right}>Favorite</Link></li>
+{/* 
                     <a href="/Home" className={styles.item_left}><span className="fa fa-tasks"></span></a>
                     <a href="/Home" className={styles.item_right}>Home</a>
                     <a href="/About" className={styles.item_right}>About</a>
-                    <a href="/Favorite" className={styles.item_right}>Favorite</a>
+                    <a href="/Favorite" className={styles.item_right}>Favorite</a> */}
                 </ul>
             </nav>
         </div>
