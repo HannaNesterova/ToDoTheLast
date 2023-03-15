d5414e3649fa238e771ebdae40d04ce54b2180e4
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-03-15 19:51:31 +0100

    Create lists.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,6 +11,7 @@ export const getFilteredCards = ({ cards, searchString }, columnId) => cards
   .filter(card => card.columnId === columnId && strContains(card.title, searchString));
 
 export const getAllColumns = (state) => state.columns;
+export const getAllLists = (state) => state.lists;
 
 // action creators
 export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
